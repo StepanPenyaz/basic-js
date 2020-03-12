8249842f06b17a07eaafbfa3a97fe23676746b0e
Stepan Penyaz <stepan_penyaz@epam.com>
2020-03-12 19:18:42 +0300

    feat: add solution for 'vigener-cipher'

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,64 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+  constructor(isReverse) {
+    this.isReverse = isReverse;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  }
+
+  encrypt(message, key) {
+    var result = '';
+    var processIndex = 0;
+
+    var messageElements = message.toUpperCase();
+    for (var index = 0; index < messageElements.length; index++) {
+      const element = messageElements[index];
+      if (this.alphabet.indexOf(element) == -1) {
+        result += element;
+      }
+      else {
+        var resultIndex = (this.alphabet.indexOf(element)
+          + this.alphabet.indexOf(key.toUpperCase()[processIndex]))
+          % this.alphabet.length;
+        processIndex++;
+
+        if (processIndex == key.length)
+          processIndex = 0;
+
+        result += this.alphabet[resultIndex];
+      }
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    if (this.isReverse == false)
+      return result.split('').reverse().join('');
+    return result;
+  }
+
+  decrypt(message, key) {
+    var result = '';
+    var processIndex = 0;
+
+    var messageElements = message.toUpperCase();
+    for (var index = 0; index < messageElements.length; index++) {
+      const element = messageElements[index];
+      if (this.alphabet.indexOf(element) == -1) {
+        result += element;
+      }
+      else {
+        var resultIndex = (this.alphabet.indexOf(element)
+          + this.alphabet.length
+          - this.alphabet.indexOf(key.toUpperCase()[processIndex]))
+          % this.alphabet.length;
+        processIndex++;
+
+        if (processIndex == key.length)
+          processIndex = 0;
+
+        result += this.alphabet[resultIndex];
+      }
     }
+    if (this.isReverse == false)
+      return result.split('').reverse().join('');
+    return result;
+  }
 }
 
 module.exports = VigenereCipheringMachine;
