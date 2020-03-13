43e732e32f88f56260205c6ab8754bbd6e8b16cf
Stepan Penyaz <stepan_penyaz@epam.com>
2020-03-13 18:02:01 +0300

    feat: add solution for 'dream-team'

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,11 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members) == false)
+    return false;
+  var result = [];
+  members.forEach(element => {
+    if (typeof element == 'string') {
+      result.push(element.trim().toUpperCase().split('').shift())
+    }
+  });
+  return result.sort().join('');
 };
