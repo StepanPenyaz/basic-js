5668a7dfb83efa8e562717bcc14db37335b87809
Stepan Penyaz <stepan_penyaz@epam.com>
2020-03-22 17:06:45 +0300

    feat: add solution for 'extended-repeater', 'hanoi-tower', 'simple-chain'

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,37 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (options.separator === undefined) {
+    options.separator = "+";
+  }
+
+  if (options.additionSeparator === undefined) {
+    options.additionSeparator = "|";
+  }
+
+  if (options.repeatTimes === undefined) {
+    options.repeatTimes = 1;
+  }
+
+  if (options.addition === undefined) {
+    options.addition = "";
+  }
+
+  if (options.additionRepeatTimes === undefined) {
+    options.additionRepeatTimes = 1;
+  }
+
+  const additionArray = [];
+
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+    additionArray.push(String(options.addition));
+  }
+
+  const addition = additionArray.join(`${options.additionSeparator}`);
+
+  const resultArray = [];
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+    resultArray.push(`${String(str)}${addition}`);
+  }
+
+  return resultArray.join(options.separator);
 };
-  
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,33 @@
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
+
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.push(`( ${value} )`);
+    return this;
   },
+
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (typeof position !== "number" || !Number.isInteger(position) || position < 1 || position > this.chain.length - 1) {
+      this.chain = [];
+      throw Error;
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
+
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    const result = [...this.chain];
+    this.chain = [];
+    return result.join("~~");
   }
 };
 
