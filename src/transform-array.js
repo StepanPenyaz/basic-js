8777bb4b2564cf1004d2cc3611f9b82d2ce7d98d
Stepan Penyaz <stepan_penyaz@epam.com>
2020-03-22 16:57:04 +0300

    feat: add solution for 'transfor-array'

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,29 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (Array.isArray(arr) == false)
+    throw new Error();
+  var resultArray = [];
+  for (let index = 0; index < arr.length; index++) {
+    switch (arr[index]) {
+      case "--discard-next":
+        index++;
+        break;
+      case "--discard-prev":
+        resultArray.pop();
+        break;
+      case "--double-next":
+        if (index < arr.length - 1) {
+          resultArray.push(arr[index + 1]);
+        }
+        break;
+      case "--double-prev":
+        if (index > 0) {
+          resultArray.push(arr[index - 1]);
+        }
+        break;
+      default:
+        resultArray.push(arr[index]);
+        break;
+    }
+  }
+  return resultArray;
 };
